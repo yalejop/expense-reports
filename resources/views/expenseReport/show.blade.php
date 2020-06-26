64fe605b332c8254849c011e2671fa6878555768
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-06-26 14:44:46 -0500

    crear vista de reportes 2.0

---
+++ resources/views/expenseReport/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="row">
+    <div class="col">
+    <h1> Report: {{ $report->title }}</h1>
+    </div>
+</div> 
+<div class="row">
+    <div class="col">
+        <a class="btn btn-secondary" href="/expense_reports/create">Back</a>    
+    </div>    
+</div>   
+<br>
+<div class="row">
+    Details...
+</div>
+@endsection
